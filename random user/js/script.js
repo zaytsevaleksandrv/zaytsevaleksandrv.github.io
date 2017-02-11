4b3f5018aa4012ea27b08cdc05d0f7eb5d30e1d2
Aleksandr Zaytsev <zaytsev.aleksandr.v@gmail.com>
2017-02-11 19:11:03 +0400

    Add files via upload

--- random user/js/script.js
+++ random user/js/script.js
@@ -21,8 +21,8 @@ getData('https://api.randomuser.me/1.0/?results=50&nat=gb,us&inc=gender,name,loc
 
         var html = '';
         for (var i=0; i < data["results"].length; i++){
-            var name = data["results"][i]["name"]["title"] + ' ' + data["results"][i]["name"]["first"] + ' ' + data["results"][i]["name"]["last"];
-            var img = data["results"][i]["picture"]["large"];
+            var name = data["results"][i]["name"]["title"] + ' ' + data["results"][i]["name"]["first"] + ' ' + data["results"][i]["name"]["last"],
+                img = data["results"][i]["picture"]["large"];
             html += '<div class="col-md-2 block__user_on" onclick="myModal(this)" id="' + [i] + '"> ';
             html += '<img src="' + img + '" title="'+ name +'"/>';
             html += '<p class="block__user_on-capitalize">' + name + '</p>';
@@ -32,9 +32,10 @@ getData('https://api.randomuser.me/1.0/?results=50&nat=gb,us&inc=gender,name,loc
     }  
 );
 
-function myModal() {
+function myModal(data) {
     document.getElementById("Modal").style.display = "block";
-    document.getElementById("userModal").innerHTML = "????";
+    document.getElementById("userModal").innerHTML =  data;
+    console.log(data);
 } 
 function closeModal(){
     document.getElementById("Modal").style.display = "none";
