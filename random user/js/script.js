b2b5f7e73a4a221125a730f770019164eaee0d7c
Aleksandr Zaytsev <zaytsev.aleksandr.v@gmail.com>
2017-02-11 17:24:19 +0400

    Add files via upload

--- random user/js/script.js
+++ random user/js/script.js
@@ -2,7 +2,6 @@ function getData(url, callback) {
     xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            console.log(xmlhttp.responseText);
             try {
                 var data = JSON.parse(xmlhttp.responseText);
             } catch(err) {
@@ -16,21 +15,27 @@ function getData(url, callback) {
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
 }
- 
-getData('https://api.randomuser.me/1.0/?results=50&nat=gb,us&inc=gender,name,location,email,phone,picture', function(data) {
 
-    var html = '';
+getData('https://api.randomuser.me/1.0/?results=50&nat=gb,us&inc=gender,name,location,email,phone,picture',
+    function(data) {
 
-    for (var i=0; i < data["results"].length; i++){
+        var html = '';
+        for (var i=0; i < data["results"].length; i++){
+            var name = data["results"][i]["name"]["title"] + ' ' + data["results"][i]["name"]["first"] + ' ' + data["results"][i]["name"]["last"];
+            var img = data["results"][i]["picture"]["large"];
+            html += '<div class="col-md-2 block__user_on" onclick="myModal(this)" id="' + [i] + '"> ';
+            html += '<img src="' + img + '" title="'+ name +'"/>';
+            html += '<p class="block__user_on-capitalize">' + name + '</p>';
+            html += '</div>';
+        } 
+        document.getElementById("user").innerHTML = html;
+    }  
+);
 
-        var name = data["results"][i]["name"]["title"] + ' ' + data["results"][i]["name"]["first"] + ' ' + data["results"][i]["name"]["last"];
-        var img = data["results"][i]["picture"]["large"];
-
-        html += '<div class="col-md-2 container__block_user_on" onclick="alert()" id="' + [i] + '"> ';
-        html += '<img src="' + img + '" title="'+ name +'"/>';
-        html += '<p class="container__block_user_on-capitalize">' + name + '</p>';
-        html += '</div>';
-    } 
-
-    document.getElementById("user").innerHTML = html;
-});
+function myModal() {
+    document.getElementById("Modal").style.display = "block";
+    document.getElementById("userModal").innerHTML = "????";
+} 
+function closeModal(){
+    document.getElementById("Modal").style.display = "none";
+}
