55e06227de74a5edf1b06d37c634952fc7645fc5
Aleksandr Zaytsev <zaytsev.aleksandr.v@gmail.com>
2017-02-11 14:24:03 +0400

    Add files via upload

--- random user/js/script.js
+++ random user/js/script.js
@@ -1,8 +1,8 @@
-function ajax_get(url, callback) {
+function getData(url, callback) {
     xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            console.log('responseText:' + xmlhttp.responseText);
+            console.log(xmlhttp.responseText);
             try {
                 var data = JSON.parse(xmlhttp.responseText);
             } catch(err) {
@@ -17,18 +17,15 @@ function ajax_get(url, callback) {
     xmlhttp.send();
 }
  
-ajax_get('https://api.randomuser.me/1.0/?results=50&nat=gb,us&inc=gender,name,location,email,phone,picture', function(data) {
-    // document.getElementById("title").innerHTML = data["results"];
-     var html;    
-    for (var i=0; i < data["results"].length; i++){
-            html += '<li>' + data["results"][i]["gender"] + '</li>'
+getData('https://api.randomuser.me/1.0/?results=50&nat=gb,us&inc=gender,name,location,email,phone,picture', function(data) {
+   
+    var html;
+        for (var i=0; i < data["results"].length; i++){
+            html += '<div class="col-md-2 container__block_user_on">';
+            html += '<img src="' + data["results"][i]["picture"]["large"] + '"/>';
+            html += '<p>' + data["results"][i]["name"]["title"] + ' ' + data["results"][i]["name"]["first"] + ' ' + data["results"][i]["name"]["last"] + '</p>';
+            html += '<p><a href="#">Подробнее...</a></p>'
+            html += '</div>';
         } 
-    // var html = "<h2>" + data["title"] + "</h2>";
-    // html += "<h3>" + data["description"] + "</h3>";
-    // html += "<ul>";
-    //    for (var i=0; i < data["articles"].length; i++) {
-    //        html += '<li><a href="' + data["articles"][i]["url"] + '">' + data["articles"][i]["title"] + "</a></li>";
-    //    }
-    // html += "</ul>";
-     document.getElementById("root").innerHTML = html;
+     document.getElementById("user").innerHTML = html;
 });
