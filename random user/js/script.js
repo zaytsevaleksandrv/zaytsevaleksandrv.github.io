62cc1ca852e3e398bd0ebe46621d76cf0b5cde28
Aleksandr Zaytsev <zaytsev.aleksandr.v@gmail.com>
2017-02-11 14:27:03 +0400

    Update script.js

--- random user/js/script.js
+++ random user/js/script.js
@@ -18,14 +18,13 @@ function getData(url, callback) {
 }
  
 getData('https://api.randomuser.me/1.0/?results=50&nat=gb,us&inc=gender,name,location,email,phone,picture', function(data) {
-   
-    var html;
-        for (var i=0; i < data["results"].length; i++){
-            html += '<div class="col-md-2 container__block_user_on">';
-            html += '<img src="' + data["results"][i]["picture"]["large"] + '"/>';
-            html += '<p>' + data["results"][i]["name"]["title"] + ' ' + data["results"][i]["name"]["first"] + ' ' + data["results"][i]["name"]["last"] + '</p>';
-            html += '<p><a href="#">Подробнее...</a></p>'
-            html += '</div>';
-        } 
-     document.getElementById("user").innerHTML = html;
+    var html = '';
+    for (var i=0; i < data["results"].length; i++){
+        html += '<div class="col-md-2 container__block_user_on">';
+        html += '<img src="' + data["results"][i]["picture"]["large"] + '"/>';
+        html += '<p>' + data["results"][i]["name"]["title"] + ' ' + data["results"][i]["name"]["first"] + ' ' + data["results"][i]["name"]["last"] + '</p>';
+        html += '<p><a href="#">Подробнее...</a></p>'
+        html += '</div>';
+    } 
+    document.getElementById("user").innerHTML = html;
 });
