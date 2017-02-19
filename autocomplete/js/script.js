3837ca8900a94cea1db3014ab439ebdbad8db297
Aleksandr Zaytsev <zaytsev.aleksandr.v@gmail.com>
2017-02-19 15:36:05 +0300

    Add files via upload

--- autocomplete/js/script.js
+++ autocomplete/js/script.js
@@ -19,9 +19,9 @@ function getData(url, callback) {
 getData('js/kladr.json', renderResults);
 function renderResults(data) {
     var html = '';
-    for ( var i = 0; i < data[""].length; i++){
-        var id = data[""].Id,
-            city = data[""].City;
+    for ( var i = 0; i < data[].length; i++){
+        var id = data[].Id,
+            city = data[].City;
         html += '<div>' + id + '</div>';
         html += '<div>' + city + '</div>';
 
