9427f716823ab4b94872cb768fb71a3ee9b2773d
Aleksandr Zaytsev <zaytsev.aleksandr.v@gmail.com>
2017-03-04 22:34:00 +0400

    Add files via upload

--- universiade/js/script.js
+++ universiade/js/script.js
@@ -1,7 +1,5 @@
 $(window).scroll(function(){
     if ( $(this).scrollTop() > 1700 ) {
         $('.block_popup').addClass('block_popup_animate');
-    } else {
-        $('.block_popup').removeClass('block_popup_animate');
     }
 })
