e94bdd6c64718f596df456f61aec6d1b3518c66e
Aleksandr Zaytsev <zaytsev.aleksandr.v@gmail.com>
2017-03-24 00:33:24 +0400

    Add files via upload

--- universiade/js/img.js
+++ universiade/js/img.js
@@ -20,17 +20,42 @@ var results = [];
 
 function renderResults(data) {
 
-    var img = '';
+     var img = '';
     for (var i = 0; i < data["img"].length; i++) {
-        var img = data["img"][i]["picture"]["large"];
-        img += '<div class="col-md-2 block__user_on" onclick="myModal(this.id)" id="' + [i] + '"> ';
-        img += '<img class="large" src="' + img + '" />';
+        var image = data["img"][i]["picture"]["large"];
+
+        img += '<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 block_video_one_mobile">'
+        img += '<div class="block_video_one" onclick="myModal(this.id)" id="' + [i] + '"> ';
+        img += ' <img class="img_max_w img_play_bg" src="' + image + '" alt="Видео"/>';
+        img += '</div>';
         img += '</div>';
     }
-    document.getElementById("user").innerHTML = img;
+    document.getElementById("video__football").innerHTML = img;
     results = data["img"];
 
 }
 
 getData('https://zaytsevaleksandrv.github.io/universiade/js/img1.json', renderResults);
 
+
+function myModal(index) {
+
+    var data = results[index];
+    document.getElementById("Modal").style.display = "block";
+    document.getElementById("Modal_sublist").style.display = "block";
+    var modal = '',
+        video = data.picture.video;
+
+    modal += '<div > ';
+    modal += '<iframe id="iframe_remove" src="' + video + '" frameborder="0" allowfullscreen></iframe>';
+    modal += '</div>';
+
+    document.getElementById("userModal").innerHTML = modal;
+
+}
+
+function closeModal() {
+    document.getElementById("Modal").style.display = "none";
+    document.getElementById("Modal_sublist").style.display = "none";
+    document.getElementById("iframe_remove").remove();
+}
