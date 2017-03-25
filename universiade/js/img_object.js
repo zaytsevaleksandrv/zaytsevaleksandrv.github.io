590bfd761815bfff7685de461b223aa7406b7879
Aleksandr Zaytsev <zaytsev.aleksandr.v@gmail.com>
2017-03-25 12:17:32 +0400

    Add files via upload

--- universiade/js/img_object.js
+++ universiade/js/img_object.js
@@ -19,20 +19,7 @@ function getData(url, callback) {
 var results = [];
 
 function renderResults(data) {
-
-     var img = '';
-    for (var i = 0; i < data["img"].length; i++) {
-        var image = data["img"][i]["picture"]["large"];
-
-        img += '<div class="col-lg-4 col-md-4 col-sm-4 col-xs-6">'
-        img += '<div class="block_video_one" onclick="myModal(this.id)" id="' + [i] + '"> ';
-        img += ' <img class="img_max_w img_play_bg" src="' + image + '" alt="Видео"/>';
-        img += '</div>';
-        img += '</div>';
-    }
-    document.getElementById("imgObject").innerHTML = img;
     results = data["img"];
-
 }
 
 getData('https://zaytsevaleksandrv.github.io/universiade/js/img_object.json', renderResults);
@@ -46,8 +33,8 @@ function myModal(index) {
     var modal = '',
         image = data.picture.large;
 
-    modal += '<div > ';
-    modal += '<img src="' + image +'" />';
+    modal += '<div> ';
+    modal += '<img class="windows_object" src="' + image +'" />';
     modal += '</div>';
 
     document.getElementById("userModal").innerHTML = modal;
