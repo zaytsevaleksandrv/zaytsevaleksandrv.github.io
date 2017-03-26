c0fe05b9248bc162f034863da4bf5b4d2891f380
Aleksandr Zaytsev <zaytsev.aleksandr.v@gmail.com>
2017-03-26 23:05:05 +0400

    Add files via upload

--- universiade/js/script.js
+++ universiade/js/script.js
@@ -21,3 +21,20 @@ $('.panel-title a').click(function(e){
         $('.arrow_down').addClass('arrow_down_rot');
     }
 });
+
+//swipe
+    $(document).ready(function() {  
+
+    //Enable swiping...
+    $(".carousel-inner").swipe( {
+        //Generic swipe handler for all directions
+        swipeLeft:function(event, direction, distance, duration, fingerCount) {
+            $(this).parent().carousel('prev'); 
+        },
+        swipeRight: function() {
+            $(this).parent().carousel('next'); 
+        },
+        //Default is 75px, set to 0 for demo so any distance triggers swipe
+        threshold:0
+    });
+});
