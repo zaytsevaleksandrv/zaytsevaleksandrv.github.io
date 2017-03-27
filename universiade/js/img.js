665a70d98f43f267738221cb2c200ac121b1474d
Aleksandr Zaytsev <zaytsev.aleksandr.v@gmail.com>
2017-03-28 01:34:02 +0400

    Add files via upload

--- universiade/js/img.js
+++ universiade/js/img.js
@@ -24,7 +24,7 @@ function renderResults(data) {
     for (var i = 0; i < data["img"].length; i++) {
         var image = data["img"][i]["picture"]["large"];
 
-        img += '<div class="block_video_one_mobile col-lg-4 col-md-4 col-sm-4 col-xs-4">'
+        img += '<div class=" full_width block_video_one_mobile col-lg-4 col-md-4 col-sm-4 col-xs-4">'
         img += '<div class="block_video_one" onclick="myModal(this.id)" id="' + [i] + '"> ';
         img += ' <img class="img_max_w img_play_bg" src="' + image + '" alt="Видео"/>';
         img += '</div>';
@@ -59,16 +59,3 @@ function closeModal() {
     document.getElementById("Modal_sublist").style.display = "none";
     document.getElementById("iframe_remove").remove();
 }
-
-$(document).ready(function(){
-var ravenous = function() { 
-    if (matchMedia(' only screen and (max-width: 749px)').matches)
-    {
-        $('.block_video_one_mobile').addClass('full_width');
-    } else {
-        $('.block_video_one_mobile').removeClass('full_width');
-    }
-};
-$(window).resize(ravenous);
-ravenous();  
-});
