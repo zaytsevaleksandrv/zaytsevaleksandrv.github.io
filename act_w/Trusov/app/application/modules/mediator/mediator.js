d84159c4c8617de4dd454e5f678633f38cc533ab
Zaytsev Aleksandr <azaitsev@phoenixit.ru>
2017-09-28 11:38:40 +0400

    25.09

---
+++ act_w/Trusov/app/application/modules/mediator/mediator.js
@@ -0,0 +1,42 @@
+function Mediator() {
+
+	// about events
+	var EVENT_TYPES = {
+		ANOTHER_EVENT: 'another',
+		SOME_EVENT: 'some_event'
+	};
+
+    var events = {};
+    
+    this.getTypes = function () {
+    	return EVENT_TYPES;
+    };
+    this.call = function (name, data) {
+    	if (events[name]) {
+    		for (var i = 0; i < events[name].length; i++) {
+    			if (events[name][i] instanceof Function) {
+    				events[name][i](data);
+    			}
+    		}
+    	}
+    };
+    this.subscribe = function (name, _func) {
+    	if (events[name] && _func instanceof Function) {
+    		events[name].push(_func);
+    	}
+    };
+    
+    // about data
+    var DATA_TYPES = {};
+    this.sendData = function() {};
+    this.getData = function() {};
+    
+    function init() {
+    	for (var key in TYPES) {
+    		events[EVENT_TYPES[key]] = [];
+    	}
+    }
+    init();
+}
+
+module.exports = Mediator;
