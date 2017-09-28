4ac03fa9b37d0ea50983132d7a18b4f4ea875362
Zaytsev Aleksandr <azaitsev@phoenixit.ru>
2017-09-28 11:38:35 +0400

    25.09

--- act_w/Trusov/app/app.js
+++ act_w/Trusov/app/app.js
@@ -4,11 +4,25 @@ var express = require('express');
 var app = express();
 
 var router = require('./application/routers/router');
+var Mediator = require('./application/modules/mediator/mediator');
+
+var mediator = new Mediator();
 
 app.use(express.static(__dirname + '/public'));
 
 app.use('/', router);
 
 var server = app.listen(3000, function() {
-  console.log('server start at port 3000');
+	console.log('server start at port 3000');
+
+	var TYPES = mediator.getTypes();
+	mediator.subscribe(TYPES.SOME_EVENT, function (data) {
+		console.log('1', data);
+	});
+	
+	mediator.subscribe(TYPES.SOME_EVENT, function (data) {
+		console.log('2', data);
+	});
+
+	mediator.call(TYPES.SOME_EVENT, 'vasya');
 });
