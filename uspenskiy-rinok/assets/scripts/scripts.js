321742de6f383e6203336e90f9723bf0aa242854
Zaytsev Aleksandr <azaitsev@phoenixit.ru>
2017-10-01 21:06:19 +0400

    freelance

--- uspenskiy-rinok/assets/scripts/scripts.js
+++ uspenskiy-rinok/assets/scripts/scripts.js
@@ -1,39 +1,47 @@
+var carouselSob = $('#carouselSob');
 $('.sob-block').click(function () {
     var imageType = parseInt($(this).attr('data-image-num'));
     $(this).parent().find('.active').removeClass('active');
     $(this).addClass('active');
-    $('#carouselSob').carousel(imageType);
+    carouselSob.carousel(imageType);
 });
-$('#carouselSob').bind('slide.bs.carousel', function (e) {
+carouselSob.bind('slide.bs.carousel', function (e) {
     var slide = e.to;
     var rightCarusel = $('#right-carusel');
     rightCarusel.find('.active').removeClass('active');
     rightCarusel.find('[data-image-num="' + slide + '"]').addClass('active');
 });
 //меню
-var offset = $("header .logo").offset().left + $("header .logo").width();
-$('.menu_horizontal').css('left', offset + 'px');
-$('.menu_horizontal').css('width', $(window).width() - offset + 'px');
+var header_logo = $("header .logo");
+var offset = header_logo.offset().left + header_logo.width();
+var menu_horizontal = $('.menu_horizontal');
+menu_horizontal.css('left', offset + 'px');
+menu_horizontal.css('width', $(window).width() - offset + 'px');
 $(window).resize(function () {
     setTimeout(function () {
-        var offset = $("header .logo").offset().left + $("header .logo").width();
-        $('.menu_horizontal').css('left', offset + 'px');
-        $('.menu_horizontal').css('width', $(window).width() - offset + 'px');
+        var menu_horizontal = $('.menu_horizontal');
+        var header_logo = $("header .logo");
+        var offset = header_logo.offset().left + header_logo.width();
+        menu_horizontal.css('left', offset + 'px');
+        menu_horizontal.css('width', $(window).width() - offset + 'px');
     }, 300);
 });
 $('.icon_status_menu').click(function () {
     $(this).parent().parent().toggleClass('open');
 });
-$('.about .slider').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    prevArrow: '.leftb',
-    variableWidth: true,
-    centerMode: true,
-    nextArrow: '.rightb',
-    autoplaySpeed: 3000
-});
+var about_slider = $('.about .slider');
+if (about_slider.length) {
+    about_slider.slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        prevArrow: '.leftb',
+        variableWidth: true,
+        centerMode: true,
+        nextArrow: '.rightb',
+        autoplaySpeed: 3000
+    });
+}
 $('.hist_ .button_year a').click(function () {
     var num = parseInt($(this).attr('data-num'));
     $(this).parent().find('.active').removeClass('active');
@@ -43,4 +51,7 @@ $('.hist_ .button_year a').click(function () {
     $('.history .images img.active').removeClass('active');
     $('.history .images img:eq('+num+')').addClass('active');
     return false;
+});
+$('.products__item').click(function () {
+    $('#productModal').modal('show');
 });
