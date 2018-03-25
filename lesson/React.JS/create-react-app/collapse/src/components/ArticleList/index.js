01e422b8680d42b9e3f590dae7b08ce28be84297
azaytsev <zaytsev.aleksandr.v@gmail.com>
2018-03-25 23:41:17 +0400

    basic course

---
+++ lesson/React.JS/create-react-app/collapse/src/components/ArticleList/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Article from '../Article'
+import './style.css'
+
+export default function ArticleList({articles}) {
+    const articleElements = articles.map( (article, index) =>
+        <li key={article.id} className="article-list__li">
+            <Article article = {article} defaultOpen = {index === 0} />
+        </li>
+    )
+
+    return <ul>
+        {articleElements}
+    </ul>
+}
