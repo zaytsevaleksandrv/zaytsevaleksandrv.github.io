b856b1679929baf7dbc60d9a650ef4cf95164b44
azaytsev <zaytsev.aleksandr.v@gmail.com>
2018-03-29 12:21:25 +0400

    Услуги

--- tectus/assets/script/main.js
+++ tectus/assets/script/main.js
@@ -1,5 +1,6 @@
 window.onscroll = function() {Sticky()};
 
+//плавающий header
 function Sticky() {
     var header = document.getElementById("myHeader"),
         sticky = header.offsetTop;
@@ -10,3 +11,44 @@ function Sticky() {
         header.classList.remove("tectus__header-sticky");
     }
 }
+
+//плавный переход, активные ссылки
+var sections = $('section')
+    , nav = $('nav')
+    , nav_height = nav.outerHeight();
+
+$(window).on('scroll', function () {
+    var cur_pos = $(this).scrollTop();
+
+    sections.each(function() {
+        var top = $(this).offset().top - nav_height,
+            bottom = top + $(this).outerHeight();
+
+        if (cur_pos >= top && cur_pos <= bottom) {
+            nav.find('a').removeClass('active');
+            sections.removeClass('active');
+
+            $(this).addClass('active');
+            nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+        }
+    });
+});
+
+nav.find('a').on('click', function () {
+    var $el = $(this)
+        , id = $el.attr('href');
+
+    $('html, body').animate({
+        scrollTop: $(id).offset().top - nav_height
+    }, 500);
+
+    return false;
+});
+
+//sliders
+$('.tectus__amenities-slider').slick({
+    infinite: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+});
