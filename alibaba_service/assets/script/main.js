5e500f45e329cd8a43b3b87233837298769896b0
azaytsev <zaytsev.aleksandr.v@gmail.com>
2018-05-16 22:03:12 +0400

    emloyees

---
+++ alibaba_service/assets/script/main.js
@@ -0,0 +1,63 @@
+//sliders
+$('.employees__slider').slick({
+    infinite: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: false,
+    responsive: [
+        {
+            breakpoint: 991,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: false,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: false,
+                dots: true,
+                centerMode: true,
+                centerPadding: '60px'
+            }
+        },
+        {
+            breakpoint: 426,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: false,
+                dots: true,
+                centerMode: true,
+                centerPadding: '80px'
+            }
+        },
+        {
+            breakpoint: 376,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: false,
+                dots: true,
+                centerMode: true,
+                centerPadding: '60px'
+            }
+        },
+        {
+            breakpoint: 321,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: false,
+                dots: true,
+                centerMode: true,
+                centerPadding: '30px'
+            }
+        }
+    ]
+});
