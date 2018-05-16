c2ae5b4d006f054002da0c076e300fe33c85d847
azaytsev <zaytsev.aleksandr.v@gmail.com>
2018-05-17 00:29:44 +0400

    gratitude

--- alibaba_service/assets/script/main.js
+++ alibaba_service/assets/script/main.js
@@ -61,3 +61,67 @@ $('.employees__slider').slick({
         }
     ]
 });
+
+$('.gratitude__slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: true,
+    dots: false,
+    centerMode: true,
+    responsive: [
+        {
+            breakpoint: 991,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: false,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: false,
+                dots: true,
+                centerMode: true,
+                centerPadding: '60px'
+            }
+        },
+        {
+            breakpoint: 426,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: false,
+                dots: true,
+                centerMode: true,
+                centerPadding: '80px'
+            }
+        },
+        {
+            breakpoint: 376,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: false,
+                dots: true,
+                centerMode: true,
+                centerPadding: '60px'
+            }
+        },
+        {
+            breakpoint: 321,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: false,
+                dots: true,
+                centerMode: true,
+                centerPadding: '30px'
+            }
+        }
+    ]
+});
