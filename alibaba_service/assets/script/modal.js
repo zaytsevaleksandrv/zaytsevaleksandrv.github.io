901bf860944eefdb2a633bc343f9ef731d6c9b7f
azaytsev <zaytsev.aleksandr.v@gmail.com>
2018-05-28 14:41:21 +0400

    fix

--- alibaba_service/assets/script/modal.js
+++ alibaba_service/assets/script/modal.js
@@ -1,6 +1,8 @@
 function ModalBalance() {
     let modal = document.querySelector('.top-up-balance');
     let modal_mobile = document.getElementById('modal_balance_mobile');
-    modal.classList.add("top-up-balance_active");
-    modal_mobile.classList.add("top-up-balance_active");
+
+    modal.classList.toggle("top-up-balance_active");
+    modal_mobile.classList.toggle("top-up-balance_active");
+
 }
