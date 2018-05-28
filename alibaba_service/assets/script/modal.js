84b2c292c17bada46c1c5aaa8f8e92467732db32
azaytsev <zaytsev.aleksandr.v@gmail.com>
2018-05-28 16:31:27 +0400

    alibs_lk_clients_balance

--- alibaba_service/assets/script/modal.js
+++ alibaba_service/assets/script/modal.js
@@ -4,5 +4,20 @@ function ModalBalance() {
 
     modal.classList.toggle("top-up-balance_active");
     modal_mobile.classList.toggle("top-up-balance_active");
+}
 
+function ModalToUpBalance() {
+    let modal = document.getElementById('modal_top_up_balance_wrapper');
+    let modal_inner = document.getElementById('modal_top_up_balance');
+    modal.classList.add("modal-wrapper_active");
+
+    this.closeModal = function() {
+        modal.classList.remove('modal-wrapper_active');
+    };
+
+    modal_inner.onclick = function(e) {
+        e.stopPropagation();
+    };
+
+    modal.addEventListener("click", this.closeModal);
 }
