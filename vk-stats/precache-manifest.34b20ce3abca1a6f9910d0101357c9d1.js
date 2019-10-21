5ead999cd561e96281440333e749041fc1f1948a
Aleksandr Zaytsev <zaytsev.aleksandr.v@gmail.com>
2019-10-21 17:30:41 +0400

    build

---
+++ vk-stats/precache-manifest.34b20ce3abca1a6f9910d0101357c9d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a9aaa61d9c60bee03b1b85d66789b2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8e33cb8006c7fc66e62",
+    "url": "/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "160b332c5f9b94093e32",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "e8e33cb8006c7fc66e62",
+    "url": "/static/js/2.2a338cd4.chunk.js"
+  },
+  {
+    "revision": "160b332c5f9b94093e32",
+    "url": "/static/js/main.9ba39e9c.chunk.js"
+  },
+  {
+    "revision": "b4eae56bbb6213480bec",
+    "url": "/static/js/runtime-main.c870621e.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
--- vk-stats/service-worker.js
+++ vk-stats/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vk-apps-stats/precache-manifest.f66d666ca33374d77ddd1a2a764b087a.js"
+  "/precache-manifest.34b20ce3abca1a6f9910d0101357c9d1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vk-apps-stats/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
