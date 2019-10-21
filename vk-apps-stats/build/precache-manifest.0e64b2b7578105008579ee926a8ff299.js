408b99af1e144fe7fef1843b792b2a3d421c6664
Aleksandr Zaytsev <zaytsev.aleksandr.v@gmail.com>
2019-10-21 16:48:08 +0400

    vk-apps-stats

---
+++ vk-apps-stats/build/precache-manifest.0e64b2b7578105008579ee926a8ff299.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d9ea2fd2d346e49469e63e299179ee1",
+    "url": "/vk-apps-stats/index.html"
+  },
+  {
+    "revision": "0b7e21c1bdb5fc773bf6",
+    "url": "/vk-apps-stats/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "179522960f7c4de95e1d",
+    "url": "/vk-apps-stats/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "0b7e21c1bdb5fc773bf6",
+    "url": "/vk-apps-stats/static/js/2.cb3f4097.chunk.js"
+  },
+  {
+    "revision": "179522960f7c4de95e1d",
+    "url": "/vk-apps-stats/static/js/main.15835e75.chunk.js"
+  },
+  {
+    "revision": "449ad7756125fb3d3c1c",
+    "url": "/vk-apps-stats/static/js/runtime-main.7e56a970.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-apps-stats/static/media/persik.4e1ec840.png"
+  }
+]);
