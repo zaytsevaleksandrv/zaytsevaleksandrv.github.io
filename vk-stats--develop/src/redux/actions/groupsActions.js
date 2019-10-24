dcb1039bff4e1ebe9a4dd1732c63f2ee74f0edd0
Aleksandr Zaytsev <zaytsev.aleksandr.v@gmail.com>
2019-10-24 15:02:50 +0400

    execute

--- vk-stats--develop/src/redux/actions/groupsActions.js
+++ vk-stats--develop/src/redux/actions/groupsActions.js
@@ -27,74 +27,49 @@ export const getGroups = (id, token) => async dispatch => {
 export const getGroup = (id, token) => async dispatch => {
   dispatch({ type: "GET_GROUP_REQ" });
 
-  async function getMembers(groupId, membersCount) {
-    let membersGroup = [];
-    const code = `let members = await connect.sendPromise("VKWebAppCallAPIMethod", {
-            method: "groups.getMembers",
-            request_id: "groupsUser",
-            params: {
-              group_id: groupId,
-              sort: "id_asc",
-              offset: membersGroup.length,
-              fields: "id,photo_50",
-              v: "5.102",
-              access_token: token
-            }
-          });
+  let membersGroups = [];
+  async function getMembers(group_id, members_count) {
+    const code = `var members = API.groups.getMembers({"group_id": ${group_id}, "v": "5.27", "sort": "id_asc", "count": "1000", "offset": '${
+      membersGroups.length
+    }'}).items; var offset = 1000; while (offset < 25000 && (offset + ${
+      membersGroups.length
+    }) < ${members_count}) { members = members + "," + API.groups.getMembers({"group_id": ${group_id}, "v": "5.27", "sort": "id_asc", "count": "1000", "offset": '${
+      membersGroups.length
+    } + offset'}).items; offset = offset + 1000;}; return members;`;
 
-          members = members.response.items;
-          let offset = 1000;
-
-          while (
-            offset < 25000 &&
-            offset + membersGroup.length < membersCount
-          ) {
-            const innerMembersWhile = await connect.sendPromise(
-              "VKWebAppCallAPIMethod",
-              {
-                method: "groups.getMembers",
-                request_id: "groupsUser",
-                params: {
-                  group_id: groupId,
-                  sort: "id_asc",
-                  offset: membersGroup.length + offset,
-                  fields: "id,photo_50",
-                  v: "5.102",
-                  access_token: token
-                }
-              }
-            );
-            members = [...members, ...innerMembersWhile.response.items];
-            offset = offset + 1000;
-          }
-
-          return members;`;
     const executePromise = await connect.sendPromise("VKWebAppCallAPIMethod", {
       method: "execute",
-      code: code
+      params: {
+        code: code,
+        access_token: token,
+        v: "5.102"
+      }
     });
-    if (executePromise.response) {
-      membersGroup = [...membersGroup, executePromise.response.items];
-      console.log("загрузка", membersGroup.length, "из", membersCount);
 
-      if (membersCount > membersGroup.length) {
-        setTimeout(() => {
-          getMembers(groupId, membersCount);
+    if (executePromise.response) {
+      membersGroups = membersGroups.concat(
+        JSON.parse("[" + executePromise.response + "]")
+      );
+      console.log("загрузка", membersGroups.length, "из", members_count);
+
+      if (members_count > membersGroups.length) {
+        setTimeout(function() {
+          getMembers(group_id, members_count);
         }, 333);
       } else {
         console.log(
           "Ура тест закончен! В массиве membersGroups теперь " +
-            membersGroup.length +
+            membersGroups.length +
             " элементов."
         );
 
-        return membersGroup;
+        return membersGroups;
       }
     } else {
-      console.log(executePromise.error.error_msg);
+      console.log(executePromise.error_msg);
     }
 
-    return membersGroup;
+    return membersGroups;
   }
 
   try {
