276e2fbc6c863f8eedbc3f1f06e6a5b1d40b8948
Aleksandr Zaytsev <zaytsev.aleksandr.v@gmail.com>
2019-10-25 23:01:42 +0400

    main

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,23 +1,8 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { Component } from "react";
+import Main from "./src/Main";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>люблю Иришку!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: 20,
-    flex: 1,
-    backgroundColor: "black",
-    alignItems: "center",
-    justifyContent: "flex-start"
-  },
-  text: {
-    color: "white"
+export default class App extends Component {
+  render() {
+    return <Main />;
   }
-});
+}
